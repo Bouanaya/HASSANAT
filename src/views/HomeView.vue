b23c839e44fbde5a3b76b40832fb5251d52e039c
Bouanaya <bouanayasoufiane@gmail.com>
2024-04-15 18:30:46 -0400

    text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -39,7 +39,7 @@
 <script setup>
 const pos = document.documentElement
  pos.addEventListener("mousemove", e=>{
-  console.log(e);
+   
   pos.style.setProperty("--x", e.clientX + "px")
    pos.style.setProperty("--y", e.clientY + "px")
 
